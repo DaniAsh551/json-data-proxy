e676f28f30ae2b92058ab28bc6d2dcaeac14ebb6
Ahmed Ashham <ahmedashham@Danis-MacBook-Pro.local>
2021-06-28 17:08:35 +0500

    feat: fs change reflection test

--- index.test.ts
+++ index.test.ts
@@ -1,11 +1,26 @@
 import JsonProxy from "./index";
 import fs from "fs";
 import { emit } from "process";
+import { threadId } from "worker_threads";
+import {promisify} from "util";
+
+const writeFileAsync = promisify(fs.writeFile);
+
+const TEST_FILE_SUFFIX = ".testdata.json";
+
+function sleep(ms:number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
+
+let lastIndex = -1;
+const getTestFilePath = () => `./${++lastIndex}${TEST_FILE_SUFFIX}`;
 
 describe("test if file is created", function () {
-    it("should create file named '[0-9]_data.json'", function () {
-        const testFile = "0_data.json";
-        const testPath = `./${testFile}`;
+    it("should create file named '[0-9].testdata.json'", function () {
+        const testPath = getTestFilePath();
+        console.log('TEST_PATH', testPath);
         var jp = new JsonProxy<Array<number>>(testPath, []);
         expect(fs.existsSync(testPath));
         let content = fs.readFileSync(testPath, { encoding:"utf8" });
@@ -14,8 +29,8 @@ describe("test if file is created", function () {
         fs.rmSync(testPath);
     });
     it("should be interactible", function () {
-        const testFile = "1_data.json";
-        const testPath = `./${testFile}`;
+        const testPath = getTestFilePath();
+        console.log('TEST_PATH', testPath);
         var jp = new JsonProxy<Array<number>>(testPath, []);
         var number = 123;
         if(!jp.proxy){
@@ -27,5 +42,51 @@ describe("test if file is created", function () {
         expect(jp.proxy[0]).toBe(number);
         jp.destroy();
         fs.rmSync(testPath);
+    });
+
+    it("should reflect FS changes", async function () {
+        const commitInterval = 10;
+        const testPath = getTestFilePath();
+        console.log('TEST_PATH', testPath);
+        var jp = new JsonProxy<Array<number>>(testPath, [], { async:false, commitInterval });
+        jp.addListener(() => console.log("CHANGED"));
+        var number = 123;
+        if(!jp.proxy){
+            jp.destroy();
+            fs.rmSync(testPath);
+            throw "proxy is inaccesible";
+        }
+
+        await writeFileAsync(testPath, JSON.stringify([number]))
+        .then(async () => {
+            await sleep(commitInterval * 100);
+            if(!jp.proxy){
+                jp.destroy();
+                fs.rmSync(testPath);
+                throw "proxy is inaccesible";
+            }
+            expect(jp.proxy[0]).toBe(number);
+            jp.destroy();
+            fs.rmSync(testPath);
+        });
+    }, 3000);
+});
+
+//prepare
+beforeAll(async () => {
+    await promisify(fs.readdir)("./").then(async files => {
+        files.filter(fPath => fPath.endsWith(TEST_FILE_SUFFIX))
+        .forEach(async (fPath) => promisify(fs.rm)(fPath));
+    })
+    .catch(e => {throw e;});
+});
+
+//cleanup
+afterAll(async () => {
+    let testFiles = Array.from(Array(lastIndex)).map((_,i) => `./${i}${TEST_FILE_SUFFIX}`);
+    testFiles.forEach(async (fPath) => {
+        await promisify(fs.stat)(fPath, { throwIfNoEntry: false }).then(async () => {
+            promisify(fs.rm)(fPath);
+        });
     });
 });
